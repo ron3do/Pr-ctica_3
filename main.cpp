519b0ce7257033020cb8d1662fa924568e3089f8
RONALD EDUARDO EDUARDO <>
2021-05-12 14:14:10 +0000

    trat

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@
 #include "string"
 using namespace std;
 int cant=0;
-struct tipo{  //se crea una estructura en la cual se va realizar los 
+struct componentes{  //se crea una estructura en la cual se va realizar los 
   string device;
   string tipo;
   float consumo;
@@ -24,24 +24,23 @@ int main() {
   float consumo1=0.0;
   float tiempo1=0.0;
   
+  
 
   cout<< "Digite la cantidad de disositivos IOT que posee"<<endl;  
   cin>>cant;
-  struct tipo iot[cant];
+  struct componentes iot[cant];  
+  
 
   
   for(int cant1=0;cant1<cant;cant1++){
     
     cout<<"Digite el nombre del dispositivo: "<<endl;
-    //cin>> disp1;
-    getline (cin,disp1);
+    getline(cin,disp1);
     iot[cant1].device=disp1;
     cout<<"Digite el nombre del tipo: "<<endl;
-    cin>> tipo1;
+    getline(cin,tipo1);
     iot[cant1].tipo=tipo1;
-    //cout<< iot[cant1].device<<endl;
-    //cout<< iot[cant1].tipo<<endl;
-    
+
 
   }
   for (int cant2=0;cant2<cant;cant2++){
