33809e04d6024aff3f7829b78fb8302647b25a40
RONALD EDUARDO EDUARDO <>
2021-05-15 15:10:25 +0000

    cambios 2

--- main.cpp
+++ main.cpp
@@ -2,11 +2,12 @@
 // Randall 
 // practica 3 
 
-#include "iostream"
+#include <iostream>
 #include "string"
 using namespace std;
 int cant=0;
-struct componentes{  //se crea una estructura en la cual se va realizar los 
+/* Se crea una estructura en la que se almacena los datos*/
+struct componentes{ 
   string device;
   string tipo;
   float consumo;
@@ -15,31 +16,31 @@ struct componentes{  //se crea una estructura en la cual se va realizar los
 }; 
 
 
-
-
 int main() {
-   
+  
+  
+  using namespace std;
   float consumo1=0.0;
   float tiempo1=0.0;
-  
-  
+  int x=0;
+  //struct componentes iot[cant];
 
   cout<< "Digite la cantidad de disositivos IOT que posee"<<endl;  
-  cin>>cant;
-  struct componentes iot[cant];  
-  
+  cin>>x;
+  cant=x;  
+  struct componentes iot[cant];
 
-  
-  for(int cant1=0;cant1<cant;cant1++){
+      for(int cant1=0;cant1<cant;++cant1){
     
     cout<<"Digite el nombre del dispositivo: "<<endl;
     getline(cin,iot[cant1].device);
-    
-    
-    cout<<"Digite el nombre del tipo: "<<endl;
+    cout<<"Digite el tipo de dispositivo que es: "<<endl;
     getline(cin,iot[cant1].tipo);
+    cout<<"Digite el consumo del dispositivo: "<<endl;
+    cin>>iot[cant1].consumo;
+  
     
-    
+  
 
 
   }
